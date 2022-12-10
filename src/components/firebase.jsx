60a5088c2d4a252a15994d775d0849155cbb9f4d
Rawisha <rawvsmaw22@gmail.com>
2022-12-10 04:47:59 +0100

    env fix2

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -2,13 +2,15 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+
+
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
+    apiKey: "AIzaSyAA9JtAO1Cejb6uBVyxPbQ7zFJiKQ7LyYU",
+    authDomain: "minimovie-c672b.firebaseapp.com",
+    projectId: "minimovie-c672b",
+    storageBucket: "minimovie-c672b.appspot.com",
+    messagingSenderId: "72700633791",
+    appId: "1:72700633791:web:a9eca27e98ed00486c1c0a"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
