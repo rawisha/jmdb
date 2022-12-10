422645d05267f2e0756ec427a54b9bc422db91ff
Rawisha <rawvsmaw22@gmail.com>
2022-12-10 04:30:55 +0100

    fix

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-console.log(process.env)
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
