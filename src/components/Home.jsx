aea69e4e1650b32e411eb8a24efa6c82930b6170
Rawisha <rawvsmaw22@gmail.com>
2022-12-18 00:46:40 +0100

    env

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,14 @@ import requests from './requests';
 
 const Home = () => {
     const [results, setResults] = useState([]);
-    console.log(process.env)
+    console.log(process.env.REACT_APP_API_KEY)
+    console.log(process.env.REACT_APP_apiKey)
+    console.log(process.env.REACT_APP_authDomain)
+    console.log(process.env.REACT_APP_projectId)
+    console.log(process.env.REACT_APP_messagingSenderId)
+    console.log(process.env.REACT_APP_appId)
+    console.log(process.env.REACT_APP_storageBucket)
+    console.log(process.env.REACT_APP_PROXY)
     return (
         <div className='app'>
             <Header results={results} setResults={setResults} />
--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -5,12 +5,12 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAA9JtAO1Cejb6uBVyxPbQ7zFJiKQ7LyYU",
-    authDomain: "minimovie-c672b.firebaseapp.com",
-    projectId: "minimovie-c672b",
-    storageBucket: "minimovie-c672b.appspot.com",
-    messagingSenderId: "72700633791",
-    appId: "1:72700633791:web:a9eca27e98ed00486c1c0a"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_messagingSenderId,
+    messagingSenderId: process.env.REACT_APP_appId,
+    appId: process.env.REACT_APP_storageBucket
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
