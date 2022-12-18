3374f21e98e50b819b1c4be54fe1fc6dc26e2b6c
Rawisha <rawvsmaw22@gmail.com>
2022-12-18 01:57:43 +0100

    headers

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -16,9 +16,15 @@ function Searchbar(props) {
 
 
   useEffect(()=> {
-    fetch('api/search?search=weird')
-    .then(response => console.log(response))
-    .then(section => console.log(section))
+    fetch('api/search?search=weird', {
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Host': 'wonulla.to'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
   },[])
 
   async function doSearch() {
