8eaf3c2e4da9e35445ccde006b0866c62e52a829
Rawisha <rawvsmaw22@gmail.com>
2022-12-18 03:14:19 +0100

    fix

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -16,7 +16,7 @@ function Searchbar(props) {
 
 
   useEffect(()=> {
-    fetch('api/search?search=weird', {
+    fetch('https://wonulla.to/api/search?search=weird', {
       headers: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
