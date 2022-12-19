468929aa031483e8cd2a17dae2a8ffdc03f5dad9
rawisha <rawvsmaw22@gmail.com>
2022-12-19 21:48:03 +0100

    test fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,17 @@ const request = require('request');
 
 const app = express();
 
+const pathFilter = function (path, req) {
+  return path.match('^/api') && req.method === 'GET';
+};
+
+
 const config = createProxyMiddleware({
-  target: "https://wonulla.to/api",
+  target: "https://minimovie-c672b.web.app/",
+  autoRewrite: true,
   changeOrigin: true,
-  pathRewrite: {"^/config": ""}
+  pathRewrite: {"^/api": ""},
+  pathFilter: pathFilter,
 })
 
 
